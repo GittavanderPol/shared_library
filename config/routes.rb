919859710f5c7d095f71945a387486e141590125
Gitta <mail@gvanderpol.nl>
2023-08-09 21:26:50 +0200

    Setup devise, user model and letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "books#index"
   resources :books
+  devise_for :users, path: "auth"
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
