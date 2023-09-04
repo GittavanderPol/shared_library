fafbafd2b2e48aa83324af0cda1e292ef246703c
Gitta <mail@gvanderpol.nl>
2023-09-04 13:43:46 +0200

    Add `within_table_row` helper method to `ApplicationSystemTestCase`

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,10 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def within_table_row(number, &block)
+    within("table tr:nth-child(#{number})") do
+      yield
+    end
+  end
 end
