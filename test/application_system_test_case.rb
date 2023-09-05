76e8e118924b64c72a4652ffaf3d47296e74a2c6
Gitta <mail@gvanderpol.nl>
2023-09-05 11:45:14 +0200

    Improve 'within_table_row_for' method using dom-id

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
-  def within_table_row(number, &block)
-    within("table tr:nth-child(#{number})") do
+  def within_table_row_for(model, &block)
+    within("##{dom_id(model)}") do
       yield
     end
   end
