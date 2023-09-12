073c26b7985e1b2919e088144fb07ef582266b01
Gitta <mail@gvanderpol.nl>
2023-09-12 16:12:53 +0200

    Add methods for testing mailer

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,4 +8,13 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
       yield
     end
   end
+
+  def find_mail_to(email)
+    ActionMailer::Base.deliveries.find { |mail| mail.to.include?(email) }
+  end
+
+  def wait_for_mail_to(email)
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    wait.until { !find_mail_to(email).nil? }
+  end
 end
