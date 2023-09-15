2603969a13e5eef4671e9475cf1e5c907698e165
Gitta <mail@gvanderpol.nl>
2023-09-15 10:45:19 +0200

    Add methods to create and use user's whatsapp number

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,20 @@
+module UsersHelper
+  def whatsapp_number(user)
+    phone = user.whatsapp
+
+    phone.gsub!("+", "")
+    phone.gsub!("-", "")
+    phone.gsub!("(", "")
+    phone.gsub!(")", "")
+
+    while phone.starts_with?("0") do
+      phone = phone[1..-1]
+    end
+
+    phone
+  end
+
+  def whatsapp_link(user)
+    "https://wa.me/#{whatsapp_number(user)}"
+  end
+end
